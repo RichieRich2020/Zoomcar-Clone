256052cac5599689eac9858d65fa7f877bdc041b
AnupKamble <sahyadrihillsagro@gmail.com>
2022-10-13 02:53:49 +0530

    Todays project  work

---
+++ booking/booking.js
@@ -0,0 +1,18 @@
+
+
+
+// var cl = document.querySelector("#btn");
+document.querySelector("#btn").addEventListener("click",myfunction);
+
+function myfunction() {
+
+    event.preventDefault();
+
+
+
+   
+     
+    location.href = "payments.html";
+}
+
+
